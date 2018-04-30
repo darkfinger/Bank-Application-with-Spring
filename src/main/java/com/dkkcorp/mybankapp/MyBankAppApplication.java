c4776c032cbc2317805a89c31e476e1f09ca9050
darkfinger <892810dkcbak@G>
2018-04-30 09:14:38 -0400

    starter

---
+++ src/main/java/com/dkkcorp/mybankapp/MyBankAppApplication.java
@@ -0,0 +1,12 @@
+package com.dkkcorp.mybankapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyBankAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyBankAppApplication.class, args);
+    }
+}
