d03300305731cec79575c7a9031e9c0b0f566ec7
darkfinger <892810dkcbak@G>
2018-05-03 17:29:51 -0400

    fixed adminCommand

--- src/main/java/com/dkkcorp/mybankapp/converter/UserAdminCommandToUserAdmin.java
+++ src/main/java/com/dkkcorp/mybankapp/converter/UserAdminCommandToUserAdmin.java
@@ -30,18 +30,6 @@ public class UserAdminCommandToUserAdmin implements Converter<UserAdminCommand,U
         final UserAdmin userAdmin=new UserAdmin();
         userAdmin.setAdminPassword(source.getAdminPassword());
         userAdmin.setPosition(source.getPosition());
-        userAdmin.setId(source.getId());
-        userAdmin.setFirstName(source.getFirstName());
-        userAdmin.setLastName(source.getLastName());
-        userAdmin.setEmail(source.getEmail());
-        userAdmin.setPassword(source.getPassword());
-        userAdmin.setPin(source.getPin());
-        userAdmin.setDateOfBirth(source.getDateOfBirth());
-        userAdmin.setDateOfSubscription(source.getDateOfSubscription());
-        source.getAccount().forEach(accountCommand -> userAdmin.getAccount().add(accountCommandToAccount.convert(accountCommand)));
-        source.getUserAddress().forEach(userAddressCommand -> userAdmin.getUserAddress().add(userAddressCommandToUserAddress.convert(userAddressCommand)));
-        source.getUserContact().forEach(userContactCommand -> userAdmin.getUserContact().add(userContactCommandToUserContact.convert(userContactCommand)));
-
         return userAdmin;
     }
 }
--- src/main/java/com/dkkcorp/mybankapp/converter/UserAdminToUserAdminCommand.java
+++ src/main/java/com/dkkcorp/mybankapp/converter/UserAdminToUserAdminCommand.java
@@ -28,18 +28,6 @@ public class UserAdminToUserAdminCommand implements Converter<UserAdmin,UserAdmi
         final UserAdminCommand userAdminCommand=new UserAdminCommand();
         userAdminCommand.setAdminPassword(source.getAdminPassword());
         userAdminCommand.setPosition(source.getPosition());
-        userAdminCommand.setId(source.getId());
-        userAdminCommand.setFirstName(source.getFirstName());
-        userAdminCommand.setLastName(source.getLastName());
-        userAdminCommand.setEmail(source.getEmail());
-        userAdminCommand.setPassword(source.getPassword());
-        userAdminCommand.setPin(source.getPin());
-        userAdminCommand.setDateOfBirth(source.getDateOfBirth());
-        userAdminCommand.setDateOfSubscription(source.getDateOfSubscription());
-        source.getAccount().forEach(account -> userAdminCommand.getAccount().add(accountToAccountCommand.convert(account)));
-        source.getUserAddress().forEach(userAddress -> userAdminCommand.getUserAddress().add(userAddressToUserAddressCommand.convert(userAddress)));
-        source.getUserContact().forEach(userContact -> userAdminCommand.getUserContact().add(userContactToUserContactCommand.convert(userContact)));
-
         return userAdminCommand;
     }
 }
